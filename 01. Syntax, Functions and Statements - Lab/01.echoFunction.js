10b95b1911632c412951a88f05ef4f9dd631ab0d
StanislavM01 <81364482+StanislavM01@users.noreply.github.com>
2022-11-18 15:06:42 +0200

    Add files via upload

---
+++ 01. Syntax, Functions and Statements - Lab/01.echoFunction.js
@@ -0,0 +1,8 @@
+function echo(input = `Mouse`) {
+    console.log(input.length)
+    console.log(input)
+
+
+
+}
+echo()
---
+++ 01. Syntax, Functions and Statements - Lab/03. largestNumber.js
@@ -0,0 +1,8 @@
+function largestNumb(numbOne, numbTwo, numbThree) {
+    let largNumb = Math.max(numbOne, numbTwo, numbThree)
+    console.log(`The largest number is ${largNumb}.`)
+
+
+
+}
+largestNumb(-3, -5, -22.5)

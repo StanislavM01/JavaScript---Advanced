eeadf5a63da291714103ea5bf1bdf816cf40cdb6
StanislavM01 <81364482+StanislavM01@users.noreply.github.com>
2022-11-18 15:06:08 +0200

    Delete Syntax, Functions and Statements - Exercise directory

--- Syntax, Functions and Statements - Exercise/01. fruit.js
+++
@@ -1,9 +0,0 @@
-function fruits(typeFruit,weight,pricePerKg){
-
-let weightInKg = weight / 1000
-let needMoney = (weightInKg * pricePerKg).toFixed(2)
-console.log(`I need $${needMoney} to buy ${weightInKg.toFixed(2)} kilograms ${typeFruit}.`)
-
-
-}
-fruits('orange', 2500, 1.80)
